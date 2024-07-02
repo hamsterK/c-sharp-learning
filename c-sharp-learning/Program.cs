72cfa1c2407bb883f54cf7da7b36a6f2cfd97ea3
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-02 21:53:10 +0200

    Add project files.

---
+++ c-sharp-learning/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(3 + 3);
+            Console.Write("Hello again");
+            Console.Write("!");
+        }
+    }
+}
+// single-line comment
+
+/* multi-line
+ * comment */
