5e3338cb4fe833cd6897957eb166776ffb059479
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-04 22:08:13 +0200

    data types, user input

--- c-sharp-learning/Program.cs
+++ c-sharp-learning/Program.cs
@@ -17,16 +17,33 @@ namespace HelloWorld
             bool myBool = true;
             string myText = "Hello";
 
-            int x = 5;
-            int y = 6;
-            Console.WriteLine(x + y);
+            int i = 5;
+            int j = 6;
+            Console.WriteLine(i + j);
 
             int x = 5, y = 6, z = 50;
             Console.WriteLine(x + y + z);
 
             int a, b, c;
             a = b = c = 50;
-        }
+
+            int myInt = 10;
+            Console.WriteLine(Convert.ToString(myInt));
+
+            double myDouble = 9.78;
+            int myIntNine = (int)myDouble;  // Manual (explicit) casting
+            Console.WriteLine(myIntNine);  // Outputs 9
+
+            Console.WriteLine("Enter username:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            // int age = Console.ReadLine(); => will cause error => Cannot implicitly convert type 'string' to 'int'
+            Console.WriteLine("Your age is: " + age);
+
+        }   
     }
 }
 // single-line comment
