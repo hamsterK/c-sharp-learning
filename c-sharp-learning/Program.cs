454ff1209aef06c6805658811a82f9c5e6c4c14a
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-04 10:30:36 +0200

    data types

--- c-sharp-learning/Program.cs
+++ c-sharp-learning/Program.cs
@@ -10,6 +10,22 @@ namespace HelloWorld
             Console.WriteLine(3 + 3);
             Console.Write("Hello again");
             Console.Write("!");
+
+            const int myNum = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x + y);
+
+            int x = 5, y = 6, z = 50;
+            Console.WriteLine(x + y + z);
+
+            int a, b, c;
+            a = b = c = 50;
         }
     }
 }
