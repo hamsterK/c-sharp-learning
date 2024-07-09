ef2ed3e2a6e14e3dce6fd3fa300e3667b17026be
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-10 00:12:56 +0200

    parameters and arguments

--- c-sharp-learning/Methods.cs
+++ c-sharp-learning/Methods.cs
@@ -8,14 +8,17 @@ namespace methods_learning
 {
     class Methods
     {
-        static void MyMethod()
+        static void MyMethod(string fname, int age)
         {
             Console.WriteLine("This just got executed");
+            Console.WriteLine(fname + " Refsnes");
+            Console.WriteLine(fname + " is " + age);
         }
 
         static void Main(string[] args)
         {
-            MyMethod();
+            MyMethod("Liam", 5);
+            MyMethod("Jenny", 8);
         }
     }
 }
