bfed658d50195908802aa963f07ff7714be8b4eb
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-10 23:43:58 +0200

    methods parameters

--- c-sharp-learning/Methods.cs
+++ c-sharp-learning/Methods.cs
@@ -8,17 +8,46 @@ namespace methods_learning
 {
     class Methods
     {
-        static void MyMethod(string fname, int age)
+        static void MyMethod(string fname, int age = 18)  // 18 - default value
         {
             Console.WriteLine("This just got executed");
             Console.WriteLine(fname + " Refsnes");
             Console.WriteLine(fname + " is " + age);
         }
 
-        static void Main(string[] args)
+        static int MySum(int x)  // returns int
+        {
+            return 5 + x;
+        }
+
+        static void MyChild(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The yougest child is: " + child3);
+        }
+
+        // method overloading - define method twice with different parameters
+        static int PlusMethod(int x, int y)
+        {
+            return x + y;
+        }
+        static double PlusMethod(double x, double y)
+        {
+            return x + y;
+        }
+
+
+        static void Main(string[] args)  // void - returns nothing
         {
             MyMethod("Liam", 5);
             MyMethod("Jenny", 8);
+            MyMethod("Tom");
+            Console.WriteLine(MySum(99));
+            MyChild(child3: "John", child1: "Liam", child2: "Liam");
+            int myNum1 = PlusMethod(8, 5);
+            double myNum2 = PlusMethod(4.3, 6.26);
+            Console.WriteLine("Int:" + myNum1);
+            Console.WriteLine("Double: " + myNum2);
+
         }
     }
 }
