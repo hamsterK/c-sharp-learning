710afe486317c5cc8a15761d76ff6aa0c8f96796
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-16 19:00:22 +0200

    abstraction and interfaces

---
+++ c-sharp-learning/Abstraction.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction_learning
+{
+    abstract class Animal  // must be inherited by derived class to be used
+    {
+        // abstract method (does not have a body - it is provided by derived class)
+        public abstract void animalSound();
+        // regular method
+        public void sleep()
+        {
+            Console.WriteLine("Zzz");
+        }
+    }
+
+    // derived class
+    class Pig : Animal
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main9(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.animalSound();   
+            myPig.sleep();
+        }
+    }
+}
---
+++ c-sharp-learning/Interface.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interface_learning
+{
+    /* An interface is a completely "abstract class", which can only contain abstract methods and properties (with empty bodies)
+    It is considered good practice to start with the letter "I" at the beginning of an interface
+    By default - abstract and public */
+    interface IAnimal
+    {
+        void animalSound();
+    }
+
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    interface IFirstInterface
+    {
+        void myMethod(); // interface method
+    }
+
+    interface ISecondInterface
+    {
+        void myOtherMethod(); // interface method
+    }
+
+    // multiple interfaces
+    class DemoClass : IFirstInterface, ISecondInterface
+    {
+        public void myMethod()
+        {
+            Console.WriteLine("Text 1");
+        }
+        public void myOtherMethod()
+        {
+            Console.WriteLine("Text 2");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main10(string[] args)
+        {
+            Pig myPig = new Pig();
+            myPig.animalSound();
+
+            DemoClass myObj = new DemoClass();
+            myObj.myMethod();
+            myObj.myOtherMethod();
+        }
+    }
+}
