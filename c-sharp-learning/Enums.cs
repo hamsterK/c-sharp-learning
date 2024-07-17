cf063363e2e8df0bde7400a1a24262c28ef91350
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-17 18:07:18 +0200

    enum and working with a file

---
+++ c-sharp-learning/Enums.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enum_learning
+{ 
+
+    class Program
+{
+    // An enum is a special "class" that represents a group of constants (unchangeable/read-only variables)
+    enum Level
+    {
+        Low,
+        Medium,
+        High
+    }
+
+    static void Main11(string[] args)
+    {
+        Level myVar = Level.Medium;
+        switch (myVar)
+        {
+            case Level.Low:
+                Console.WriteLine("Low level");
+                break;
+            case Level.Medium:
+                Console.WriteLine("Medium level");
+                break;
+            case Level.High:
+                Console.WriteLine("High level");
+                break;
+        }
+    }
+}
+}
---
+++ c-sharp-learning/Files.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace files_learning
+{
+    class Program
+    {
+        static void Main12(string[] args)
+        {
+            string writeText = "Hello there!";
+            File.WriteAllText("filename.txt", writeText);
+
+            string readText = File.ReadAllText("filename.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
