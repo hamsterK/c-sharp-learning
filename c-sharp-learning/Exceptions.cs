1ba6e536eebb4bd9cd70735a7cfbe143fea216cc
Anastasiya Krauchuk <anastasiya.krauchuk96@gmail.com>
2024-07-18 22:45:43 +0200

    exceptions

---
+++ c-sharp-learning/Exceptions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptions_learning
+{
+    class Program
+    {
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access denied - You must be at least 18 years old.");
+            }
+            else
+            {
+                Console.WriteLine("Access granted - You are old enough!");
+            }
+        }
+        static void Main13(string[] args)
+        {
+            checkAge(20);
+
+            try
+            {
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Sorry about that!");
+                Console.WriteLine(e.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("The 'try catch' is finished.");
+            }
+        }
+    }
+
+}
